5a3326c32731b6cf97e6c9049758abffae9c79a4
James Dindi <dindijjames@gmail.com>
2017-10-14 22:46:58 +0300

    Created class Employee

---
+++ employee.py
@@ -0,0 +1,36 @@
+# Python Object Oriented Programming
+
+
+class Employee:
+
+    num_of_emps = 0
+    raise_amount = 1.04
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@company.com'
+
+        Employee.num_of_emps += 1
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amount)
+
+
+
+emp_1 = Employee('Corey', 'Schafer', 50000)
+emp_2 = Employee('Test', 'User', 60000)
+
+print(emp_1.email)
+print(emp_1.fullname())
+print(emp_1.pay)
+emp_1.apply_raise()
+print(emp_1.pay)
+
+print(Employee.num_of_emps)
+
+
