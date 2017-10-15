bdc37d8ef76603bf403ef9e32d45e4a3ff1e31ee
James Dindi <dindijjames@gmail.com>
2017-10-15 05:23:00 +0300

    Illustrating class methods

--- employee.py
+++ employee.py
@@ -20,17 +20,45 @@ class Employee:
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amount)
 
+    @classmethod
+    def set_raise_amt(cls, amount):
+        cls.raise_amount = amount
+
+     #Alternative class constructor
+    @classmethod
+    def from_string(cls, emp_str):
+        first, last, pay = emp_str.split('-')
+        return cls(first, last, pay)
+
+    '''@staticmethod
+    def is_workday(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        return True'''
 
 
-emp_1 = Employee('Corey', 'Schafer', 50000)
-emp_2 = Employee('Test', 'User', 60000)
+dev_1 = Employee('Corey', 'Schafer', 50000)
+dev_2 = Employee('Test', 'User', 60000)
 
-print(emp_1.email)
-print(emp_1.fullname())
-print(emp_1.pay)
-emp_1.apply_raise()
-print(emp_1.pay)
+print(dev_1.email)
+print(dev_2.email)
 
-print(Employee.num_of_emps)
+'''
+import datetime
+my_date = datetime.date(2017, 10, 17)
+
+print(Employee.is_workday(my_date))'''
+
+emp_str_1 = 'John-Doe-70000'
+emp_str_2 = 'Steve-Smith-30000'
+emp_str_3 = 'Jame-Doe-90000'
+
+new_emp_1 = Employee.from_string(emp_str_1)
+
+print(new_emp_1.email)
+print(new_emp_1.pay)
+
+
+
 
 
