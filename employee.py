ca4d64a6d50aa6a291406e85c9d00987895173a4
James Dindi <dindijjames@gmail.com>
2017-10-16 19:57:36 +0300

    Added two sub classes Developer and Manager, extending class Employee

--- employee.py
+++ employee.py
@@ -42,25 +42,47 @@ class Employee:
 
 class Developer(Employee):
     """Class Developer inheriting from Employee class"""
-    pass
+    raise_amount = 1.10
 
-dev_1 = Employee('Corey', 'Schafer', 50000)
-dev_2 = Employee('Test', 'User', 60000)
+    def __init__(self, first, last, pay, prog_lang):
+        '''Redefining the Employer init method to 
+        include the prog_lang too'''
+        super().__init__(first, last, pay)
+        self.prog_lang = prog_lang
 
-print(dev_1.email)
+class Manager(Employee):
+    """Class Manager extends Employee"""
+    def __init__(self, first, last, pay, employees = None):
+        '''Redefining the Employer init method to 
+        include the employees managed by manager'''
+        super().__init__(first, last, pay)
+        if employees = None:
+            self.employees = []
+        else:
+            self.employees = employees
+    
+    def add_emp(self, emp):
+        '''Method to define how employees are added to manager'''
+        if emp not in self.employees:
+            self.employees.append(emp)
+
+    def remove_emp(self, emp):
+        '''Method to define how employees are removed from manager'''
+        if emp in self.employees:
+            self.employees.remove(emp)
+
+    def print_emps(self):
+        '''Method to print employees managed by manager'''
+        for emp in self.employees:
+            print('-->', emp.fullname())
+
+
+dev_1 = Developer('Corey', 'Schafer', 50000, 'Python')
+dev_2 = Developer('Test', 'User', 60000, 'JavaScript')
+
+mgr_1 = Manager('Sue', 'Smith', 90000, [dev_1])
+
+'''print(dev_1.email)
 print(dev_2.email)
 
-
-import datetime
-my_date = datetime.date(2017, 10, 17)
-
-print(Employee.is_workday(my_date))
-
-emp_str_1 = 'John-Doe-70000'
-emp_str_2 = 'Steve-Smith-30000'
-emp_str_3 = 'Jame-Doe-90000'
-
-new_emp_1 = Employee.from_string(emp_str_1)
-
-print(new_emp_1.email)
-print(new_emp_1.pay)
+print(dev_1.prog_lang)'''
