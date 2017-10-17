5ca6f30a77ecc2890218131f2d9150585b33f2c9
James Dindi <dindijjames@gmail.com>
2017-10-17 04:40:27 +0300

    Illustration on setters, deleters etc

--- employee.py
+++ employee.py
@@ -7,43 +7,42 @@ class Employee:
     num_of_emps = 0
     raise_amount = 1.04
 
-    def __init__(self, first, last, pay):
+    def __init__(self, first, last):
         self.first = first
         self.last = last
-        self.pay = pay
-        self.email = first + '.' + last + '@company.com'
+    
+    @property
+    def email(self):
+        """Find email format
+        The @property decorator allows us to access the class method
+        just like we would an attribute"""
+        return '{}.{}'.format(self.first, self.last)
 
-        Employee.num_of_emps += 1
-
+    @property
     def fullname(self):
-        """Find full name fomat"""
+        """Find full name format"""
         return '{} {}'.format(self.first, self.last)
 
-    def apply_raise(self):
-        """Find raise of salary"""
-        self.pay = int(self.pay * self.raise_amount)
+    @fullname.setter
+    def fullname(self, name):
+        """Allows us to alter the first, last names and email of employee"""
+        first, last = name.split(' ')
+        self.first = first
+        self.last = last
 
-    def __repr__(self):
-        '''Returning a string that can be used to recreate an object
-        Mostly used by fellow developers'''
-        return "Employee('{}', '{}', '{}')".format(self.first, self.last, self.pay)
-
-    def __str__(self):
-        '''Meant to be more readable for the end user
-        These two methods allow us to change how our objects are printed
-        and dispalyed'''
-        return '{} - {}'.format(self.fullname(), self.email)
-
-    def __add__(self, other):
-        '''When we add two employees together, it will give us combined pay'''
-        return self.pay + other.pay
-
-    def __len__(self):
-        '''Returns number of characters in employee,s name'''
-        return len(self.fullname())
+    @fullname.deleter
+    def fullname(self):
+        """Allows us to delete the first, last names and email of employee"""
+        print("Delete Name!")
+        self.first = None
+        self.last = None
         
-emp_1 = Employee('Corey', 'Schafer', 50000)
-emp_2 = Employee('Test', 'User', 60000)
+emp_1 = Employee('John', 'Smith')
 
-print(emp_1 + emp_2)
-print(len(emp_1))
+emp_1.fullname = "Corey Schafer"
+
+print(emp_1.first)
+print(emp_1.email)
+print(emp_1.fullname)
+
+del emp_1.fullname
