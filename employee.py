bcb78f2a4ca31aef1d07682f8b4adbf409142935
James Dindi <dindijjames@gmail.com>
2017-10-17 03:56:38 +0300

    Illustrating dunder methods

--- employee.py
+++ employee.py
@@ -23,73 +23,26 @@ class Employee:
         """Find raise of salary"""
         self.pay = int(self.pay * self.raise_amount)
 
-    @classmethod
-    def set_raise_amt(cls, amount):
-        cls.raise_amount = amount
+    def __repr__(self):
+        '''Returning a string that can be used to recreate an object
+        Mostly used by fellow developers'''
+        return "Employee('{}', '{}', '{}')".format(self.first, self.last, self.pay)
 
-    #Alternative class constructor
-    @classmethod
-    def from_string(cls, emp_str):
-        first, last, pay = emp_str.split('-')
-        return cls(first, last, pay)
+    def __str__(self):
+        '''Meant to be more readable for the end user
+        These two methods allow us to change how our objects are printed
+        and dispalyed'''
+        return '{} - {}'.format(self.fullname(), self.email)
+        
+emp_1 = Employee('Corey', 'Schafer', 50000)
+emp_2 = Employee('Test', 'User', 60000)
 
-    @staticmethod
-    def is_workday(day):
-        if day.weekday() == 5 or day.weekday() == 6:
-            return False
-        return True
+print(emp_1)
 
+repr(emp_1)
+str(emp_1)
 
-class Developer(Employee):
-    """Class Developer inheriting from Employee class"""
-    raise_amount = 1.10
-
-    def __init__(self, first, last, pay, prog_lang):
-        '''Redefining the Employer init method to 
-        include the prog_lang too'''
-        super().__init__(first, last, pay)
-        self.prog_lang = prog_lang
-
-class Manager(Employee):
-    """Class Manager extends Employee"""
-    def __init__(self, first, last, pay, employees = None):
-        '''Redefining the Employer init method to 
-        include the employees managed by manager'''
-        super().__init__(first, last, pay)
-        if employees == None:
-            self.employees = []
-        else:
-            self.employees = employees
-    
-    def add_emp(self, emp):
-        '''Method to define how employees are added to manager'''
-        if emp not in self.employees:
-            self.employees.append(emp)
-
-    def remove_emp(self, emp):
-        '''Method to define how employees are removed from manager'''
-        if emp in self.employees:
-            self.employees.remove(emp)
-
-    def print_emps(self):
-        '''Method to print employees managed by manager'''
-        for emp in self.employees:
-            print('-->', emp.fullname())
-
-
-dev_1 = Developer('Corey', 'Schafer', 50000, 'Python')
-dev_2 = Developer('Test', 'User', 60000, 'JavaScript')
-
-mgr_1 = Manager('Sue', 'Smith', 90000, [dev_1])
-print(mgr_1.email)
-mgr_1.add_emp(dev_2)
-mgr_1.remove_emp(dev_1)
-mgr_1.print_emps()
-
-print(isinstance(mgr_1, Manager))
-print(issubclass(Developer, Employee))
-
-'''print(dev_1.email)
-print(dev_2.email)
-
-print(dev_1.prog_lang)'''
+'''Dunder methods can also be used to perform arithmetic calculation'''
+print(1 + 2)
+print(int.__add__(1,2))
+print(str.__add__('a', 'b')) #string __add__ concatenates the strings
